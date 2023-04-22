2924158508358c27fb04fb74721fe903d0ce7c12
Matteo Fontana <matteofontana@matteofontana.app>
2023-04-22 18:21:45 +0200

    First Implementation of the code. Rotation of a Red Rectangle (200x200) with one finger. Value of rotation is stored. No other value is stored

--- One-Finger-Rotation-Without-Value/ContentView.swift
+++ One-Finger-Rotation-Without-Value/ContentView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+struct RotatableElement: View {
+    @State private var rotation: Angle = .zero
+    @GestureState private var gestureRotation: Angle = .zero
+
+    var body: some View {
+        Rectangle()
+            .fill(Color.red)
+            .frame(width: 200, height: 200)
+            .rotationEffect(rotation + gestureRotation)
+            .gesture(
+                DragGesture()
+                    .updating($gestureRotation) { value, state, _ in
+                        let centerX = value.startLocation.x - 100
+                        let centerY = value.startLocation.y - 100
+                        
+                        let startVector = CGVector(dx: centerX, dy: centerY)
+                        
+                        let endX = value.startLocation.x + value.translation.width - 100
+                        let endY = value.startLocation.y + value.translation.height - 100
+                        let endVector = CGVector(dx: endX, dy: endY)
+                        
+                        let angleDifference = atan2(startVector.dy * endVector.dx - startVector.dx * endVector.dy, startVector.dx * endVector.dx + startVector.dy * endVector.dy)
+                        state = Angle(radians: -Double(angleDifference))
+                    }
+                    .onEnded { value in
+                        let centerX = value.startLocation.x - 100
+                        let centerY = value.startLocation.y - 100
+                        
+                        let startVector = CGVector(dx: centerX, dy: centerY)
+                        
+                        let endX = value.startLocation.x + value.translation.width - 100
+                        let endY = value.startLocation.y + value.translation.height - 100
+                        let endVector = CGVector(dx: endX, dy: endY)
+                        
+                        let angleDifference = atan2(startVector.dy * endVector.dx - startVector.dx * endVector.dy, startVector.dx * endVector.dx + startVector.dy * endVector.dy)
+                        rotation = rotation + Angle(radians: -Double(angleDifference))
+                    }
+            )
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        RotatableElement()
     }
 }
 
