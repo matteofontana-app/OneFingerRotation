44b40c4a755d0c6daeea93bd4de172926d598e2c
Matteo Fontana <matteofontana@matteofontana.app>
2023-04-22 18:14:39 +0200

    Initial Commit

---
+++ One-Finger-Rotation-Without-Value/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  One-Finger-Rotation-Without-Value
+//
+//  Created by Matteo Fontana on 22/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ One-Finger-Rotation-Without-Value/One_Finger_Rotation_Without_ValueApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_Finger_Rotation_Without_ValueApp.swift
+//  One-Finger-Rotation-Without-Value
+//
+//  Created by Matteo Fontana on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct One_Finger_Rotation_Without_ValueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
