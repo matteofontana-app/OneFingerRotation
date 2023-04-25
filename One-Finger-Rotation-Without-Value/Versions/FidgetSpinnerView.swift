503e5e1aebfbc86083855f2549de6fa10fef7993
Matteo Fontana <matteofontana@matteofontana.app>
2023-04-25 15:29:06 +0200

    Completed the code, reformat the sliders, commented all the main gesture functions.

--- One-Finger-Rotation-Without-Value/Versions/FidgetSpinnerView.swift
+++ One-Finger-Rotation-Without-Value/Versions/FidgetSpinnerView.swift
@@ -1,35 +1,48 @@
 //
-//  RotatableElement.swift
-//  One-Finger-Rotation
+//  FidgetSpinnerView.swift
+//  Fidget Spinner View
 //
 //  Created by Matteo Fontana on 23/04/23.
 //
 import SwiftUI
 
 struct FidgetSpinnerView: View {
+    /// Variable for the angle of rotation of the Fidget Spinner
     @State private var rotationAngle: Angle = .zero
+    /// Variable for the gesture rotation
     @GestureState private var gestureRotation: Angle = .zero
+    /// Variable for the last speed of the finger on the screen
     @State private var lastVelocity: CGFloat = 0
+    /// Variable to check if the Fidget Spinner is spinning
     @State private var isSpinning = false
+    /// Variable for the timer of the spinning action
     @State private var timer: Timer?
 
+    /// Variable for friction, useful for slider
     @State var friction: CGFloat = 0.994
-
-    // Add a threshold value
-    let rotationThreshold: CGFloat = 1.0
-
-    // Add a velocity multiplier
+    
+    /// Velocity multiplier
     @State var velocityMultiplier: CGFloat = 0.1
+
+    /// Threshold value
+    let rotationThreshold: CGFloat = 1.0
 
     var body: some View {
         VStack {
+            
             Spacer()
+            
+            /// Geometry Reader is necessary for the use of the gesture
             GeometryReader { geometry in
                 Image("FidgetSpinner")
                     .resizable()
                     .frame(width: 400, height: 400)
                     .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                
+                    /// The ".rotationEffect" modifier is necessary for the gesture functions
                     .rotationEffect(rotationAngle + gestureRotation, anchor: .center)
+                
+                    /// The ".gesture" modifier is necessary for the gesture functions
                     .gesture(
                         DragGesture(minimumDistance: 0)
                             .updating($gestureRotation) { value, state, _ in
@@ -65,27 +78,35 @@ struct FidgetSpinnerView: View {
 
                             }
                     )
+                
+                    /// The ".onAppear" modifier is necessary for the gesture functions
                     .onAppear {
                         timer?.invalidate()
                     }
+                
+                    /// The ".onDisappear" modifier is necessary for the gesture functions
                     .onDisappear {
                         timer?.invalidate()
                     }
             }
+            
             Spacer()
-            VStack {
-                Text("Friction: \((friction-0.9)*1000, specifier: "%.0f")")
-                    .padding(.bottom, 4)
-                Slider(value: $friction, in: 0.90...0.999)
-                    .padding([.leading, .trailing], 16)
-                Text("Velocity Multiplier: \(velocityMultiplier, specifier: "%.1f")")
-                    .padding(.bottom, 4)
-                Slider(value: $velocityMultiplier, in: 0.1...5.0)
-                    .padding([.leading, .trailing], 16)
-            }
+            
+            /// The two sliders for the friction and the Velocity Multiplier
+            Text("Friction: \((friction-0.8)*1000, specifier: "%.3f")")
+                .padding(.bottom, 4)
+            Slider(value: $friction, in: 0.80...0.999)
+                .padding([.leading, .trailing], 16)
+            Text("Velocity Multiplier: \(velocityMultiplier, specifier: "%.3f")")
+                .padding(.bottom, 4)
+            Slider(value: $velocityMultiplier, in: 0.001...5.000)
+                .padding([.leading, .trailing], 16)
+            
+            Spacer()
         }
     }
 
+    /// Function to calculate the angle rotation of the Fidget Spinner while moving and at the end of a gesture interaction
     private func calculateRotationAngle(value: DragGesture.Value, geometry: GeometryProxy) -> Angle {
         let centerX = value.startLocation.x - geometry.size.width / 2
         let centerY = value.startLocation.y - geometry.size.height / 2
@@ -106,5 +127,3 @@ struct FidgetSpinnerView_Previews: PreviewProvider {
         FidgetSpinnerView()
     }
 }
-
-//The implementation of this code works, but is there a way to create two sliders? One for the friction and one for the velocity multiplier?
