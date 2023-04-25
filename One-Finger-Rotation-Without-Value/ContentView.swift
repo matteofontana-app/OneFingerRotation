cdc55e80f46f22beb58c99bd57f8b460c6f519d6
Matteo Fontana <matteofontana@matteofontana.app>
2023-04-25 14:56:34 +0200

    Added sliders to the Fidget Spinner View to control friction and velocity multiplier.

--- One-Finger-Rotation-Without-Value/ContentView.swift
+++ One-Finger-Rotation-Without-Value/ContentView.swift
@@ -15,7 +15,6 @@ struct ContentView: View {
         //ValueRotation()
         //AutoRotation()
         
-        //RotatableElementInertia()
         FidgetSpinnerView()
     }
 }
--- One-Finger-Rotation-Without-Value/Versions/FidgetSpinnerView.swift
+++ One-Finger-Rotation-Without-Value/Versions/FidgetSpinnerView.swift
@@ -13,62 +13,76 @@ struct FidgetSpinnerView: View {
     @State private var isSpinning = false
     @State private var timer: Timer?
 
-    var friction: CGFloat = 0.994
+    @State var friction: CGFloat = 0.994
 
     // Add a threshold value
     let rotationThreshold: CGFloat = 1.0
 
     // Add a velocity multiplier
-    let velocityMultiplier: CGFloat = 0.1
+    @State var velocityMultiplier: CGFloat = 0.1
 
     var body: some View {
-        GeometryReader { geometry in
-            Image("FidgetSpinner")
-                .resizable()
-                .frame(width: 400, height: 400)
-                .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
-                .rotationEffect(rotationAngle + gestureRotation, anchor: .center)
-                .gesture(
-                    DragGesture(minimumDistance: 0)
-                        .updating($gestureRotation) { value, state, _ in
-                            state = calculateRotationAngle(value: value, geometry: geometry)
-                        }
-                        .onChanged { _ in
-                            timer?.invalidate()
-                        }
-                        .onEnded { value in
-                            let angleDifference = calculateRotationAngle(value: value, geometry: geometry)
-                            rotationAngle = rotationAngle + angleDifference
-                            let velocity = CGPoint(x: value.predictedEndLocation.x - value.location.x, y: value.predictedEndLocation.y - value.location.y)
-                            lastVelocity = sqrt(pow(velocity.x, 2) + pow(velocity.y, 2)) * velocityMultiplier
-
-                            if abs(velocity.x) > rotationThreshold || abs(velocity.y) > rotationThreshold {
-                                isSpinning = true
-                                timer?.invalidate()
-                                timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
-                                    let rotationDirection = angleDifference >= .zero ? 1.0 : -1.0
-                                    let angle = Angle(degrees: Double(lastVelocity) * rotationDirection)
-                                    rotationAngle += angle
-                                    lastVelocity *= friction
-
-                                    if lastVelocity < 0.1 {
-                                        timer.invalidate()
-                                        isSpinning = false
-                                    }
-                                }
-                            } else {
-                                timer?.invalidate()
-                                isSpinning = false
+        VStack {
+            Spacer()
+            GeometryReader { geometry in
+                Image("FidgetSpinner")
+                    .resizable()
+                    .frame(width: 400, height: 400)
+                    .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                    .rotationEffect(rotationAngle + gestureRotation, anchor: .center)
+                    .gesture(
+                        DragGesture(minimumDistance: 0)
+                            .updating($gestureRotation) { value, state, _ in
+                                state = calculateRotationAngle(value: value, geometry: geometry)
                             }
+                            .onChanged { _ in
+                                timer?.invalidate()
+                            }
+                            .onEnded { value in
+                                let angleDifference = calculateRotationAngle(value: value, geometry: geometry)
+                                rotationAngle = rotationAngle + angleDifference
+                                let velocity = CGPoint(x: value.predictedEndLocation.x - value.location.x, y: value.predictedEndLocation.y - value.location.y)
+                                lastVelocity = sqrt(pow(velocity.x, 2) + pow(velocity.y, 2)) * velocityMultiplier
 
-                        }
-                )
-                .onAppear {
-                    timer?.invalidate()
-                }
-                .onDisappear {
-                    timer?.invalidate()
-                }
+                                if abs(velocity.x) > rotationThreshold || abs(velocity.y) > rotationThreshold {
+                                    isSpinning = true
+                                    timer?.invalidate()
+                                    timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
+                                        let rotationDirection = angleDifference >= .zero ? 1.0 : -1.0
+                                        let angle = Angle(degrees: Double(lastVelocity) * rotationDirection)
+                                        rotationAngle += angle
+                                        lastVelocity *= friction
+
+                                        if lastVelocity < 0.1 {
+                                            timer.invalidate()
+                                            isSpinning = false
+                                        }
+                                    }
+                                } else {
+                                    timer?.invalidate()
+                                    isSpinning = false
+                                }
+
+                            }
+                    )
+                    .onAppear {
+                        timer?.invalidate()
+                    }
+                    .onDisappear {
+                        timer?.invalidate()
+                    }
+            }
+            Spacer()
+            VStack {
+                Text("Friction: \((friction-0.9)*1000, specifier: "%.0f")")
+                    .padding(.bottom, 4)
+                Slider(value: $friction, in: 0.90...0.999)
+                    .padding([.leading, .trailing], 16)
+                Text("Velocity Multiplier: \(velocityMultiplier, specifier: "%.1f")")
+                    .padding(.bottom, 4)
+                Slider(value: $velocityMultiplier, in: 0.1...5.0)
+                    .padding([.leading, .trailing], 16)
+            }
         }
     }
 
@@ -92,3 +106,5 @@ struct FidgetSpinnerView_Previews: PreviewProvider {
         FidgetSpinnerView()
     }
 }
+
+//The implementation of this code works, but is there a way to create two sliders? One for the friction and one for the velocity multiplier?
