e6347cd0e7e225a882774a27df91c216afee2896
matteofontanaunina <113596724+matteofontanaunina@users.noreply.github.com>
2023-05-13 15:30:41 +0200

    Availability in WatchOS 7

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "OneFingerRotation",
     platforms: [
-        .iOS(.v14), .macOS(.v11), .watchOS(.v6)
+        .iOS(.v14), .macOS(.v11), .watchOS(.v7)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
