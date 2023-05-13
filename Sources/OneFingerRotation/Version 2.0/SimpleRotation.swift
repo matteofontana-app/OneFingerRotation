e6ff8439a33913fb08514a2c96c3d79edb4fa8a7
matteofontanaunina <113596724+matteofontanaunina@users.noreply.github.com>
2023-05-13 16:14:14 +0200

    Fixed Bug in Simple Rotation

--- Sources/OneFingerRotation/Version 2.0/SimpleRotation.swift
+++ Sources/OneFingerRotation/Version 2.0/SimpleRotation.swift
@@ -32,14 +32,11 @@ public struct SimpleRotation: ViewModifier {
     }
     
     public func calculateRotation(value: DragGesture.Value) -> Angle {
-        let centerX = value.startLocation.x - 100
-        let centerY = value.startLocation.y - 100
-        let startVector = CGVector(dx: centerX, dy: centerY)
-        let endX = value.startLocation.x + value.translation.width - 100
-        let endY = value.startLocation.y + value.translation.height - 100
-        let endVector = CGVector(dx: endX, dy: endY)
-        let angleDifference = atan2(startVector.dy * endVector.dx - startVector.dx * endVector.dy, startVector.dx * endVector.dx + startVector.dy * endVector.dy)
-        var rotation = Angle(radians: -Double(angleDifference))
+        let startVector = CGVector(dx: value.startLocation.x - 180, dy: value.startLocation.y - 180)
+        let endVector = CGVector(dx: value.location.x - 180, dy: value.location.y - 180)
+
+        let angleDifference = atan2(endVector.dy, endVector.dx) - atan2(startVector.dy, startVector.dx)
+        var rotation = Angle(radians: Double(angleDifference))
         
         // Apply angle snapping if specified
         if let snap = angleSnap {
@@ -55,8 +52,7 @@ public struct SimpleRotation: ViewModifier {
 public extension View {
     func simpleRotation(
         rotationAngle: Angle? = nil,
-        angleSnap: Binding<Double?> = .constant(nil)
-    ) -> some View {
+        angleSnap: Binding<Double?> = .constant(nil)) -> some View {
         let effect = SimpleRotation(
             rotationAngle: rotationAngle ?? .degrees(0.0),
             angleSnap: angleSnap
